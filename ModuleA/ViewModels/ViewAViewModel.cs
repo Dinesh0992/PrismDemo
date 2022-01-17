978ccce3b1c920227dad3f87b550f66089744dd0
Dinesh.V <dineshviswes@gmail.com>
2022-01-17 22:42:16 +0530

    till  Demo: using ObservesCanExecute Method video

--- ModuleA/ViewModels/ViewAViewModel.cs
+++ ModuleA/ViewModels/ViewAViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Commands;
+using Prism.Mvvm;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,11 +8,45 @@ namespace ModuleA.ViewModels
 {
    public  class ViewAViewModel :BindableBase
     {
-        private string _title="Hello from ViewAViewModel";
+        private string _text="Hello from ViewAViewModel";
 
-        public string Title
-        { get { return _title; }
-          set { SetProperty(ref _title, value); }
+        public string Text
+        { get { return _text; }
+          set { SetProperty(ref _text, value); }
+        }
+
+        private bool _ChkClick=false;
+
+        public bool ChkClick
+        {
+            get { return _ChkClick; }
+            set { SetProperty(ref _ChkClick, value);
+              //  one of the method to call CanClick method of button for change in chkClick checkbox status
+                //ClickCommand.RaiseCanExecuteChanged(); 
+            }
+        }
+
+        public DelegateCommand ClickCommand { get; private set; }
+
+        public ViewAViewModel()
+        {
+            //using observable property 
+             ClickCommand = new DelegateCommand(Click, CanClick)
+                 .ObservesProperty(()=>ChkClick);
+
+            // using ObservesCanExecute ,  for this CanClick method should be commented 
+            //    ClickCommand = new DelegateCommand(Click).ObservesCanExecute(()=>ChkClick);
+        }
+        
+        private bool CanClick()
+        {
+            return ChkClick;
+        }
+        
+
+        private void Click()
+        {
+            Text ="You Clicked Me!..";
         }
     }
 }
